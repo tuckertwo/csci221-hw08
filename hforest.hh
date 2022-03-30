0a0aa58ed7fdf0ce9dd1ff70f395fc62ed0517e9
Tucker R. Twomey <tucker@the-twomeys.com>
2022-03-30 14:05:01 -0700

    Fix indentation

--- hforest.hh
+++ hforest.hh
@@ -5,19 +5,23 @@
 class HForest
 {
  public:
-   HForest();
-   ~HForest();
+  HForest();
+  ~HForest();
 
-   // Get the size of the forest
-   // /i.e./ how many trees are in the heap.
-   int size() const;
+  // Get the size of the forest
+  // /i.e./ how many trees are in the heap.
+  int size() const;
 
-   // Add a tree to the forest/heap
-   void add_tree(HTree::tree_ptr_t tree);
+  // Add a tree to the forest/heap
+  void add_tree(HTree::tree_ptr_t tree);
 
-   // Remove a tree from the forest/heap
-   HTree::tree_ptr_t pop_tree();
+  // Remove a tree from the forest/heap and return its value.
+  // Returns nullptr if the forest is empty (or, rather, clearcut).
+  HTree::tree_ptr_t pop_tree();
 
+  // Make the == operator intuitively work, mostly for testing
+  // purposes.
+  bool operator==(HForest b);
  private:
   std::vector<HTree::tree_ptr_t> forest_;
 };
