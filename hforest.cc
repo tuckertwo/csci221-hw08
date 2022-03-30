325816d80aeaa99aeda00bb687343d728bb8248c
Tucker R. Twomey <tucker@the-twomeys.com>
2022-03-30 01:30:11 -0700

    Write constructor and destructor

--- hforest.cc
+++ hforest.cc
@@ -4,3 +4,14 @@
 #include <algorithm>
 
 using namespace std;
+
+// Constructor
+// Warning: when exploring South American forests, beware of the
+// Boa Constructor.
+HForest::HForest()
+{
+  vector vector<HTree::tree_ptr_t> HForest::forest_ = {};
+}
+
+// Destructor (unremarkable)
+HForest::~HForest() {}
