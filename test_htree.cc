91ea3acc9d864b133f03a7f3fbde45bd1912043c
Tucker R. Twomey <tucker@the-twomeys.com>
2022-03-30 00:18:56 -0700

    Fix tests

--- test_htree.cc
+++ test_htree.cc
@@ -6,6 +6,7 @@
 #include <cassert>
 
 using namespace std;
+using Direction = HTree::Direction;
 
 int main()
 {
@@ -13,7 +14,7 @@ int main()
 
   // Constructing trees
   HTree::tree_ptr_t leaf    = make_shared<HTree>(16, 65536);
-  tree_ptr_t sequoia = make_shared<HTree>(154, 65536,
+  HTree::tree_ptr_t sequoia = make_shared<HTree>(154, 65536,
     make_shared<HTree>(0, 1, make_shared<HTree>(1, 2,
         make_shared<HTree>(2, 4, nullptr, leaf))),
     make_shared<HTree>(4, 7, make_shared<HTree>(8, 256),
@@ -21,33 +22,35 @@ int main()
 
   // Accessing things from trees
   assert(leaf);
-  assert(leaf.get_key() == 16);
-  assert(leaf.get_value() == 65536);
-  assert(leaf.get_child(Direction::LEFT) == nullptr);
-  assert(leaf.get_child(Direction::RIGHT) == nullptr);
+  assert(leaf->get_key() == 16);
+  assert(leaf->get_value() == 65536);
+  assert(leaf->get_child(Direction::LEFT) == nullptr);
+  assert(leaf->get_child(Direction::RIGHT) == nullptr);
   assert(sequoia);
-  assert(get_key(sequoia)   == 154);
-  assert(get_value(sequoia) == 65536);
+  assert(sequoia->get_key()   == 154);
+  assert(sequoia->get_value() == 65536);
 
   // Deeper tree traversals
-  assert(sequoia.get_child(Direction::LEFT).get_child(Direction::LEFT)
-      .get_child(Direction::LEFT));
-  assert(sequoia.get_child(Direction::LEFT).get_child(Direction::LEFT)
-      .get_child(Direction::LEFT).get_key() == 2);
-  assert(sequoia.get_child(Direction::LEFT).get_child(Direction::LEFT)
-      .get_child(Direction::LEFT).get_child(Direction::LEFT_)  == nullptr);
-  assert(sequoia.get_child(Direction::LEFT).get_child(Direction::LEFT)
-      .get_child(Direction::LEFT).get_child(Direction::RIGHT_) == leaf);
+  assert(sequoia->get_child(Direction::LEFT)->get_child(Direction::LEFT)
+      ->get_child(Direction::LEFT));
+  assert(sequoia->get_child(Direction::LEFT)->get_child(Direction::LEFT)
+      ->get_child(Direction::LEFT)->get_key() == 2);
+  assert(sequoia->get_child(Direction::LEFT)->get_child(Direction::LEFT)
+      ->get_child(Direction::LEFT)->get_child(Direction::LEFT)  == nullptr);
+  assert(sequoia->get_child(Direction::LEFT)->get_child(Direction::LEFT)
+      ->get_child(Direction::LEFT)->get_child(Direction::RIGHT) == leaf);
 
   // Finding particular elements
-  assert(leaf.path_to(16) == "");
-  assert(leaf.path_to(32) == "-");
-  assert(sequoia.path_to(32) == "-");
-  assert(sequoia.path_to(154) == "");
-  assert(sequoia.path_to(16) == "LLLR");
+  assert(leaf->path_to(16)->size()==0);
+  assert(leaf->path_to(32) == nullptr);
+  assert(sequoia->path_to(32) == nullptr);
+  assert(sequoia->path_to(154)->size()==0);
+  HTree::path_t path = {Direction::LEFT, Direction::LEFT,
+      Direction::LEFT, Direction::RIGHT};
+  assert(*(sequoia->path_to(16)) == path);
 
   // Testing node_at
-  assert(sequoia.node_at("") == sequoia);
-  assert(sequoia, path_to(sequoia.path_to(154)) == sequoia);
-  assert(sequoia, path_to(sequoia.path_to(16))  == leaf);
+  assert(sequoia->node_at(HTree::path_t()) == sequoia);
+  assert(sequoia->node_at(*(sequoia->path_to(154))) == sequoia);
+  assert(sequoia->node_at(*(sequoia->path_to(16)))  == leaf);
 }
