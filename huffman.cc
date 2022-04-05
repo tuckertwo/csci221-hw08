8e539e6c9378e95ac8e6e1dfc6a0014c6fe29785
Tucker R. Twomey <tucker@the-twomeys.com>
2022-04-05 09:55:39 -0700

    Write constructor

--- huffman.cc
+++ huffman.cc
@@ -5,6 +5,13 @@
 using namespace std;
 
 Huffman()
+{
+  vector<int> freqtab_(Huffman::ALPHABET_SIZE, 0);
+
+  // This gives EOF a frequency of 1 and then builds our initial tree.
+  this->update_freq(Huffman::HEOF);
+}
+
 void Huffman::update_freq(int symbol)
 bits_t Huffman::encode(int symbol)
 int Huffman::decode(bool bit)
